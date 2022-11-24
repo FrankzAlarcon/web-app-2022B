449e32903c25d1893e62265a0ccb675a6459c479
Frankz <leninalarcon7@gmail.com>
2022-11-24 10:51:37 -0500

    html, css, js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const nombre = prompt("Ingrese su nombre:")
+//const nombre = prompt("Ingrese su nombre:")
 
-document.querySelector(".name").textContent = nombre
+//document.querySelector(".name").textContent = nombre
+
+document.addEventListener('DOMContentLoaded', () => {
+    const para = document.querySelector('p');
+
+    para.addEventListener('click', updateName);
+
+    function updateName() {
+    let name = prompt('Enter a new name');
+    para.textContent = `Player 1:  ${name}`;
+    }
+
+})
